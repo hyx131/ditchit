e5126a452d7e76c8b8c2b4051984a5f42a5e5fea
hyx131 <selin.hyx@gmail.com>
2020-02-01 10:09:24 -0500

    survey - general logic and state set up for survey questions

---
+++ src/Components/Survey/condition.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography, Fab } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  header: {
+    margin: theme.spacing(5)
+  },
+  body: {
+    textAlign: 'center',
+    margin: theme.spacing(5)
+  },
+  button: {
+    width: theme.spacing(20),
+    backgroundColor: '#34eba1'
+  },
+  option: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(5)
+  }
+}));
+
+
+const Condition = ({ setFinishSurvey, selectedVals, setSelectedVals, ...props }) => {
+  const classes = useStyles()
+
+  const [option, setOption] = useState(true)
+
+  const clickNext = () => {
+    option === 'yes' && setSelectedVals({
+      ...selectedVals,
+      canDonate: false
+    })
+
+    option === 'no' && setSelectedVals({
+      ...selectedVals,
+      canDonate: true
+    })
+
+    setFinishSurvey(true)
+  }
+
+  const clickYes = () => {
+    setOption('yes')
+  }
+
+  const clickNo = () => {
+    setOption('no')
+  }
+
+  return (
+    <Grid container spacing={5}>
+      <Grid item xs={12} className={classes.header}>
+        <Typography align='center' variant='h6'>Are there any visible / physical damages ?</Typography>
+      </Grid>
+      <Grid item xs={12} className={classes.body}>
+        <Fab className={classes.option} onClick={clickYes} style={option === true ? null : option === 'yes' ? { backgroundColor: '#34eba1' } : null}>YES</Fab>
+        <Fab className={classes.option} onClick={clickNo} style={option === true ? null : option === 'no' ? { backgroundColor: '#34eba1' } : null}>NO</Fab>
+      </Grid>
+      <Grid item xs={12} style={{ textAlign: 'center' }}>
+        <Fab disabled={option === true} className={classes.button} variant='extended' onClick={clickNext}>Go</Fab>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Condition
--- src/Components/Survey/index.js
+++ src/Components/Survey/index.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Location from './location'
+import ItemType from './item-type'
+import Condition from './condition'
+
+//mock data:
+const itemTypes = [
+  { name: 'Clothing', src: '/public/images/test.png' },
+  { name: 'Bedding', src: '' },
+  { name: 'Accessories', src: '' },
+  { name: 'Shoes', src: '' },
+]
 
 const Survey = () => {
+  const [selectedVals, setSelectedVals] = useState({
+    location: null,
+    itemType: null,
+    conditions: [],
+    canDonate: true
+  })
+
+  const [finishSurvey, setFinishSurvey] = useState(false)
+
+  console.log(selectedVals)
   return (
-    <div>
-      Questions Page
-    </div>
+    <>
+      {!selectedVals.location && <div>
+        <Location selectedVals={selectedVals} setSelectedVals={setSelectedVals} />
+      </div>}
+
+      {selectedVals.location && selectedVals.location.city && selectedVals.location.address && selectedVals.location.postalCode && selectedVals.location.country && !selectedVals.itemType && <div>
+        <ItemType itemTypes={itemTypes} selectedVals={selectedVals} setSelectedVals={setSelectedVals} />
+      </div>}
+
+      {selectedVals.location && selectedVals.location.city && selectedVals.location.address && selectedVals.location.postalCode && selectedVals.location.country && selectedVals.itemType && !finishSurvey && <div>
+        <Condition setFinishSurvey={setFinishSurvey} selectedVals={selectedVals} setSelectedVals={setSelectedVals} />
+      </div>}
+
+    </>
   )
 }
 
