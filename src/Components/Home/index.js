73377db65eacb7ef594f2d80de4575f530e77e89
Mariam Majeed <mariam.majeed.kes@gmail.com>
2020-02-01 22:29:41 -0500

    added dynamic item option on homepage

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import { withRouter } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
 
-import tradePic from '../../Images/pluto-10.png'
+import tradePic from "../../Images/pluto-10.png";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Fab, Typography } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Fab, Typography } from "@material-ui/core";
+
+const ditchItemValueOptions = ["clothing", "bedding", "shoes", "accessories"];
+// const [ditchItemIndex, updateDitchItem] = useState(0);
 
 const HomePage = ({ history, themeColor, ...props }) => {
   const useStyles = makeStyles(theme => ({
@@ -17,29 +20,90 @@ const HomePage = ({ history, themeColor, ...props }) => {
       backgroundColor: themeColor.themeGreen
     }
   }));
-  const classes = useStyles()
+  const classes = useStyles();
+  let [index, setIndex] = useState(0);
+  // let [ditchItem, setDitchItem] = useState("clothing");
+
+  // const updateDitchItem = () => {
+  //   // if (index < 3) {
+  //   //   // console.log("index < 3 ", index);
+  //   //   setIndex(index + 1);
+  //   // } else {
+  //   //   setIndex(0);
+  //   // }
+  //   console.log("update!", index);
+  //   setIndex(index + 1);
+  // };
+
+  // let updateIndexAtInterval = setInterval(() => updateDitchItem(), 2000);
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     updateDitchItem(index);
+  //   }, 1000);
+
+  //   return () => {
+  //     console.log("unmount");
+  //     // setIndex(0);
+  //     clearInterval(id);
+  //   };
+  // }, []);
+  // const newIndex = () => {
+  //   if (index < 3) {
+  //     setIndex(index + 1);
+  //     console.log("update index");
+  //   }
+  // };
+
+  // const bob = "help";
+  // const bob = "me";
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("MY INDEX", index);
+      // setDitchItem(ditchItemValueOptions[index]);
+      if (index < 3) {
+        setIndex(index => index + 1);
+      } else {
+        setIndex(0);
+      }
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [index]);
 
   return (
-    <Grid container spacing={2} className={classes.root} justify='center'>
+    <Grid container spacing={2} className={classes.root} justify="center">
       <Grid item xs={12}>
-        <Typography align='center' variant='h5' style={{ marginTop: '10px' }} >ditch.it</Typography>
+        <Typography align="center" variant="h5" style={{ marginTop: "10px" }}>
+          ditch.it
+        </Typography>
       </Grid>
-      <Grid item xs={12} style={{ marginTop: '20px' }}>
-        <Typography align='center' variant='h3' >Take care of your <u>clothing</u></Typography>
+      <Grid item xs={12} style={{ marginTop: "20px" }}>
+        <Typography align="center" variant="h3">
+          Take care of your{" "}
+          <u id="updateItem">{ditchItemValueOptions[index]}</u>
+        </Typography>
       </Grid>
-      <Grid item xs={12} style={{ maxWidth: '500px', marginTop: '15px' }}>
-        <Typography align='center' variant='h6' >Find the right organizations to take your old stuff, personalized for you.</Typography>
+      <Grid item xs={12} style={{ maxWidth: "500px", marginTop: "15px" }}>
+        <Typography align="center" variant="h6">
+          Find the right organizations to take your old stuff, personalized for
+          you.
+        </Typography>
       </Grid>
-      <Grid item xs={12} style={{ textAlign: 'center', marginTop: '20px' }}>
-        <Fab className={classes.button} variant='extended' onClick={() => history.push('/survey')}>Start Now</Fab>
+      <Grid item xs={12} style={{ textAlign: "center", marginTop: "20px" }}>
+        <Fab
+          className={classes.button}
+          variant="extended"
+          onClick={() => history.push("/survey")}
+        >
+          Start Now
+        </Fab>
       </Grid>
-      <Grid item xs={12} style={{ marginTop: '50px' }}>
-        <Typography align='center'>
-          <img src={tradePic} alt='trade' width='30%' heigh='30%' />
+      <Grid item xs={12} style={{ marginTop: "50px" }}>
+        <Typography align="center">
+          <img src={tradePic} alt="trade" width="30%" heigh="30%" />
         </Typography>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default withRouter(HomePage)
+export default withRouter(HomePage);
